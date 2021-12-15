511b24dc684b8dc182ece6636623035f6e131b04
alamindevms <alamin.mistrisolutions@gmail.com>
2021-12-15 12:57:59 +0600

    build

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="container mx-auto px-4 lg:px-0 text-center">
-		<p class="text-sm py-4 text-blueGray-900">Copyright@2021 || Al Amin Hossain</p>
+		<p class="text-sm py-4 text-blueGray-900">Copyright@2021 || All Right Reserved By Al Amin</p>
 	</div>
 </template>
 
